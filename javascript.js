9c626b3cabf9e1be47c7225a26086c8c3fe84321
Clifton-TS <cliftontsantos@gmail.com>
2023-01-09 01:53:08 -0300

    Add whole game

--- javascript.js
+++ javascript.js
@@ -1,15 +1,32 @@
-function getComputerChoice() {
-    let num = Math.ceil(Math.random() * 3);
-    if (num == 1) {
-        return "rock"
-    }else if (num == 2) {
-        return "paper"
-    }else if (num == 3) {
-        return "scissors"
-    }
-}
-
 function playRound() {
+
+    function getComputerChoice() {
+        let num = Math.ceil(Math.random() * 3);
+        if (num == 1) {
+            return "rock"
+        }else if (num == 2) {
+            return "paper"
+        }else if (num == 3) {
+            return "scissors"
+        }
+    }
+
+    function getPlayerChoice () {
+        let keepGoing = true 
+        while(keepGoing == true) {
+        let choice = prompt("Rock, Paper or Scissors?");
+        
+        if (choice.toLowerCase() === "rock" || choice.toLowerCase() === "paper" || choice.toLowerCase() === "scissors") {
+            let keepGoing = false
+            return choice.toLowerCase()
+        }else {
+            alert("Not a choice, try again.")
+        }
+        }
+    }
+
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
     if (computerChoice === playerChoice) {
         return "It's a tie"
     }else if (computerChoice === "rock" && playerChoice === "paper") {
@@ -27,7 +44,45 @@ function playRound() {
     }
 }
 
-let playerChoice = "rock"
-let computerChoice = getComputerChoice()
+let computerWins = 0;
+let playerWins = 0;
 
-console.log(playRound())
+function game() {
+    for (let counter = 0; counter < 5; counter++) {
+        let result = playRound();
+        if (result.slice(4, 6) == "wi") {
+            console.log("Player: " + playerWins + " Computer: " + computerWins);
+            console.log(result);
+            playerWins++
+        }else if (result.slice(4, 6) == "lo") {
+            console.log("Player: " + playerWins + " Computer: " + computerWins);
+            console.log(result);
+            computerWins++
+        }else if (result.slice(4, 6) == " a") {
+            console.log("Player: " + playerWins + " Computer: " + computerWins);
+            console.log(result);
+        }else {console.log(result + " is not an option!")}
+    }
+}
+
+function announceWinner(){
+    alert("Final result: Player: " + playerWins + " Computer: " + computerWins )
+
+    if (computerWins > playerWins) {
+        alert("YOU LOST")
+    }else if (computerWins < playerWins) {
+        alert("YOU WON")
+    }else {alert("Nobody won, everyone lost...")}
+}
+
+function playAgain() {
+    if (confirm("Do you want to play again?") == true) {
+        location.reload()
+    }else {
+        alert("Alright...")
+    }
+}
+
+game()
+announceWinner()
+playAgain()
